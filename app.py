859ae6cd4980babc33ca3fb51282fcf461f6e0df
chisa-f <kiki4537fer2cat@gmail.com>
2023-03-25 19:06:50 -0400

    Add UI

---
+++ app.py
@@ -0,0 +1,29 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+
+#Define windows
+class MainWindow(Screen):
+    pass
+
+#Screen manager
+class WindowManager(ScreenManager):
+    pass
+
+class MainApp(MDApp):
+    def build(self):
+        '''Red', 'Pink', 'Purple', 'DeepPurple', '
+        Indigo', 'Blue', 'LightBlue', 'Cyan',
+        'Teal', 'Green', 'LightGreen', 'Lime',
+        'Yellow', 'Amber', 'Orange', 'DeepOrange',
+        'Brown', 'Gray', 'BlueGray'''
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.accent_palette = "Red"
+        kv = Builder.load_file('app.kv')
+        return kv
+
+
+MainApp().run()
