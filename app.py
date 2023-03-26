644b615c6c0dcacd86104519a2dbce726ca21c1f
Oren <oren.b.anderson@gmail.com>
2023-03-26 09:46:58 -0400

    Adding small functionality

--- app.py
+++ app.py
@@ -58,6 +58,8 @@ class PromptifyApp(MDApp):
         super().__init__(**kwargs)
         self.data_doggo = datadog.DataDog()
         self.fresh_data = True
+        self.last_song = ""
+        self.user_name = "user"
 
     def build(self):
         Window.bind(on_request_close=self.on_request_close)
@@ -74,11 +76,19 @@ class PromptifyApp(MDApp):
         kv = Builder.load_file('app.kv')
         return kv
 
-    def on_load(self):
+    def on_load(self, name, password):
+        if name == "" or password == "":
+            return
+
+        self.root.ids.account_screen.ids.name.text = name
+
         self.root.current = 'chat'
         # If the screen is empty, add a prompt to the chat list
         if len(self.root.ids.main_screen.ids.chatlist.children) == 0:
-            self.add_message("Voice Assistant", "Hello, ask me a question!")
+            self.add_message("Assistant", "Hello, ask me a question!")
+
+        # Add the name of the user to a locally stored variable
+        self.user_name = name
 
         # Load TTS thread
         self.data_doggo.run_stt()
@@ -92,7 +102,7 @@ class PromptifyApp(MDApp):
 
             # If the data is fresh, make a new prompt
             if self.fresh_data:
-                self.add_message("User", response[0])
+                self.add_message(self.user_name, response[0])
                 self.fresh_data = False
             else:
                 if response[1]:
@@ -107,7 +117,10 @@ class PromptifyApp(MDApp):
             self.fresh_data = True
 
         # If a song starts playing, switch screens
-        self.on_transition()
+        song = ""
+        if song not in self.last_song:
+            self.last_song = song
+            self.on_transition()
 
     def add_message(self, name, text):
         CGPT = "Assistant"
@@ -149,16 +162,16 @@ class PromptifyApp(MDApp):
     def on_transition(self):
         if self.root.current == 'chat':
             # Update the screen before we move to it
-            self.api_name()
+            self.api_name("test")
 
             # Will be used for album covers
-            self.change_picture()
+            self.change_picture("test")
 
             # Will be used for song names
-            self.primary_text()
+            self.primary_text("test")
 
             # Will be used for artist names
-            self.secondary_text()
+            self.secondary_text("test")
 
             # Move to the screen
             self.get_running_app().screen_direction = "left"
