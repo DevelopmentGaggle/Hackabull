fdb25786674dbafc607aeb222fe9918aebf82674
StevenPhang22 <sphang@ufl.edu>
2023-03-25 23:20:11 -0400

    fed the dog

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import spotify_engine2
+import speechToText_engine
+import datadog
+
+dog = datadog.DataDog()
+dog.run_stt()
 """
 uris, names, artists = spotify_engine.spotify_GetSongRecommendations()
 print(uris)
@@ -9,10 +14,13 @@ playlistID = spotify_engine.spotify_CreateNewPlaylist()
 spotify_engine.spotify_AddSongsToPlaylist(playlistID, uris)
 """
 
+
+
+
 #print(spotify_engine2.sp.current_user())
 #print([sub['id'] for sub in spotify_engine2.sp.search('artist:Taylor Swift', limit=10, type='track')['tracks']['items']])
 #print(spotify_engine2.sp.user_playlist_create(spotify_engine2.sp.current_user()['display_name'], 'Party Time', public='True', collaborative='False', description=''))
 #sp.user_playlist_add_tracks(sp.current_user()['uri'], sp.user_playlist_create(sp.current_user()['display_name'], 'Party Time', public='True', collaborative='False', description='')['id'], [sub['id'] for sub in sp.search('artist:Taylor Swift', limit=10, type='track')['tracks']['items']])
-spotify_engine2.sp.user_playlist_add_tracks(spotify_engine2.sp.current_user()['uri'], spotify_engine2.sp.user_playlist_create(spotify_engine2.sp.current_user()['display_name'], 'Party Time', public='True', collaborative='False', description='')['id'], [sub['id'] for sub in spotify_engine2.sp.search('artist:Taylor Swift', limit=10, type='track')['tracks']['items']])
+#spotify_engine2.sp.user_playlist_add_tracks(spotify_engine2.sp.current_user()['uri'], spotify_engine2.sp.user_playlist_create(spotify_engine2.sp.current_user()['display_name'], 'Party Time', public='True', collaborative='False', description='')['id'], [sub['id'] for sub in spotify_engine2.sp.search('artist:Taylor Swift', limit=10, type='track')['tracks']['items']])
 
 #Create a new playlist called party time, and add 10 taylor swift songs
