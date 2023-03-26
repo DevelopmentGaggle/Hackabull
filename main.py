df7812e5a4b3ff65e551304f65d7a525c1b9633d
StevenPhang22 <sphang@ufl.edu>
2023-03-26 11:24:11 -0400

    fixed album covers

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from spotify_engine2 import sp as sp
 import datadog
 import spotipy
 
-#print(sp.current_user_recently_played(limit=1)['items'][0]['track']['artists'][0]['name'])
-print(sp.currently_playing()['item']['album']['images'][0]['url'])
+print(sp.current_user_recently_played(limit=1)['items'][0]['track']['album']['images'][0]['url'])
+#print(sp.currently_playing()['item']['album']['images'][0]['url'])
 #sp.user_playlist_add_tracks(sp.current_user()['uri'], sp.user_playlist_create(sp.current_user()['display_name'], 'Soup Time', public='True', collaborative='False', description='')['id'], [sub['id'] for sub in sp.search('genre:pop', limit=15, type='track')['tracks']['items']])
 #dog = datadog.DataDog()
 #dog.run_stt()
